ecb705a1b3cae5cc6c33936eff4f6eff0f533d4d
kd8lvt <kd8lvt@kd8lvt.com>
2025-05-07 18:01:06 -0400

    Track the same data, per total battle size

--- datasci.py
+++ datasci.py
@@ -3,8 +3,8 @@ import json
 def newBattlerData(allChars,target):
         vs = {}
         for char in allChars:
-            if (char.name != target): vs.setdefault(char.name,{"wins":0,"losses":0,"matches":0})
-        return {"matches":0,"wins":0,"vs":vs}
+            if (char.name != target): vs.setdefault(char.name,{"wins":0,"winsMatchSize":[0,0,0,0,0,0,0,0,0,0,0,0],"losses":0,"lossesMatchSize":[0,0,0,0,0,0,0,0,0,0,0,0],"matches":0,"matchesMatchSize":[0,0,0,0,0,0,0,0,0,0,0,0]})
+        return {"matches":0,"matchesMatchSize":[0,0,0,0,0,0,0,0,0,0,0,0],"wins":0,"winsMatchSize":[0,0,0,0,0,0,0,0,0,0,0,0],"lossesMatchSize":[0,0,0,0,0,0,0,0,0,0,0,0],"vs":vs}
 
 def read():
     with open("data.json","r") as file:
@@ -28,6 +28,7 @@ def write(allChars,participants,winner):
     init(allChars)
     data = read()
     for battler in participants:
+        matchSize = len(participants)
         if battler.name not in data.keys():
             data.setdefault(battler.name,newBattlerData(allChars,battler.name))
 
@@ -35,16 +36,21 @@ def write(allChars,participants,winner):
         won = battler.name == winner
 
         bData["matches"]+=1
+        bData["matchesMatchSize"][matchSize]+=1
         if won: 
             bData["wins"]+=1
+            bData["winsMatchSize"][matchSize]+=1
 
         for other in participants:
             if other.name == battler.name: continue
             bData["vs"][other.name]["matches"]+=1
+            bData["vs"][other.name]["matchesMatchSize"][matchSize]+=1
             if won:
                 bData["vs"][other.name]["wins"]+=1
+                bData["vs"][other.name]["winsMatchSize"][matchSize]+=1
             elif winner == other.name:
                 bData["vs"][other.name]["losses"]+=1
+                bData["vs"][other.name]["lossesMatchSize"][matchSize]+=1
         data[battler.name] = bData
         
     with open("data.json","w") as file:
